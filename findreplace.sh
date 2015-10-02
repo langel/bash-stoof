8847113736e3e5d5310fac19c3818038c5f5ad8a
langel <knoxburry@gmail.com>
2015-10-02 01:42:12 +0000

    recursive file string replacer

---
+++ findreplace.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NEEDLE=$1
+REPLACEMENT=$2
+
+echo "Recursive String Replacement"
+if [ $# -eq 0 ]
+then
+	echo "usage :: .//findreplace.sh needle replacement"
+else
+	STRING="s/${NEEDLE}/${REPLACEMENT}/g"
+	echo $STRING
+	find . -type f -exec sed -i $STRING {} +
+fi
