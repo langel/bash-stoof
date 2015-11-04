60cf497a2104c013842247092f564735a0129a28
Jason Langel <jason.langel@nimboxx.com>
2015-11-04 11:49:12 -0600

    install-tools updates motd

--- install-tools.sh
+++ install-tools.sh
@@ -5,4 +5,5 @@ sudo apt-get install sl
 sudo apt-get install cowsay
 sudo apt-get install toilet
 sudo cp 69-motu-id /etc/update-motd.d/
+sudo run-parts /etc/update-motd.d/
 toilet "good job" | cowsay -W 69
