c8fa52016d2c48b0fea0898ad1d303743a5291d8
puke7 <knoxburry@gmail.com>
2015-12-13 19:04:47 -0600

    quit pane and cleaner cow toilet

--- afk.sh
+++ afk.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+clear
 text=$1
 while [ 1 ]
 do
 COLUMNS=$(tput cols)
+tput cup 0 0
 cowsay "$text" | toilet --gay --width "$COLUMNS"
 len=${#text}
 new_text=${text:1:$len}${text:0:1}
