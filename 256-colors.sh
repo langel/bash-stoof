a2c616db4f9f9de93d800335e142ebb96f12f7f7
Langel <jason.langel@saplinglearning.com>
2017-04-07 02:19:39 -0500

    256-colors.sh is beautiful

--- 256-colors.sh
+++ 256-colors.sh
@@ -2,19 +2,23 @@
 # simple grid display of all 256 bash terminal colors
 
 echo #Newline Cinema Space
-echo -e "\033[0m Foreground Colors :: \\\033[38;5;{color}m\t\t\t Background Colors :: \\\033[48;5;{color}m"
-echo #Newline Cinema Space
-color=0
-for x_counter in {0..31} ; do
-  fg=""
-  bg=""
-  for y_counter in {0..7} ; do
-    fg="${fg}\033[38;5;${color}m ${color}\t\033[0m"
-    bg="${bg}\033[48;5;${color}m \033[38;5;239m${color}\t\033[0m"
-    ((color++))
-  done
-  echo -e $fg$bg
-done
+printf " Foreground Colors :: \\\033[38;5;{color}m"
+printf "                    Background Colors :: \\\033[48;5;{color}m\n"
 echo #Newline Cinema Space
 
-exit 0
+fgcolor=0;
+bgcolor=0;
+for x_counter in {0..31} ; do
+  for y_counter in {0..7} ; do
+    printf "\033[38;5;%dm%5d  \033[0m" $fgcolor $fgcolor
+    ((fgcolor++))
+  done
+  printf "   "
+  for y_counter in {0..7} ; do
+    printf "\033[48;5;%dm\033[38;5;0m%5d  \033[0m" $bgcolor $bgcolor
+    ((bgcolor++))
+  done
+  echo  
+done
+
+echo #Newline Cinema Space
